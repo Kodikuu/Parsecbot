a7a41517dc71649b515a020847d6e3b86c7481c3
Kodikuu <cw16432@essex.ac.uk>
2019-01-07 21:49:24 +0000

    Implement auto-restarting bootstrapper script.
    Run the bot from bootstrap.py, it will keep it online in the event of issues.

---
+++ bootstrap.py
@@ -0,0 +1,16 @@
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    interpreter = sys.executable  # Fetch python executable path
+    cmd = (interpreter, "main.py")  # Create main.py execution command
+
+    print("Executing Bot initialisation.")
+    while True:
+        code = subprocess.call(cmd)  # Execute bot initialisation, return code
+        if not code == 0x78:  # 0x78 == 01111000 == x, eXit
+            print(f'\nRe-executing Bot initialisation. Exit code {code}')
+            continue
+        else:  # Exit if bot script returns special shutdown code 0x78
+            print(f'\nShutting down. Exit code {code}')
+            break
